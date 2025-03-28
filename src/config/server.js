edcd8f3a62688278fa2eb05d9127cade75e79e5b
Kasuma Dana <kasumadana06@gmail.com>
2025-03-28 10:03:52 +0800

    Add files via upload
    Menambahkan struktur dasar file

---
+++ src/config/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const routes = require('../api/routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5000,
+    host: process.env.HOST || 'localhost',
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  });
+
+  // Register routes
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+init();
